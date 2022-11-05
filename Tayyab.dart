ae3718798ddf334442f75a20d577ada9095f14d9
Tayyab Sheikh <tayyabshikh34@gmail.com>
2022-11-05 22:12:30 +0500

    Work Complete

---
+++ Tayyab.dart
@@ -0,0 +1,109 @@
+void main() {
+  // Question#01
+  // int length = 10;
+  // int breadth = 13;
+  // if (length == breadth) {
+  //   print("This is square ");
+  // } else {
+  //   print("This is rectangle");
+  // }
+
+  //Question#02
+  // int a=18;
+  // int b=116;
+  // int c=14;
+  // if( a>b &&a>c )
+  // {
+  //   print ("${a}  is  youngest ");
+  // }
+  // else if (b>a &&b>c)
+  // {
+  //   print("${b} is youngest ");
+  // }
+  // else if (c>a &&c>b)
+  // {
+  //   print ("${c} is youngest ");
+  // }
+  // else
+  // {
+  //   print ("A,B and C is oldest value");
+  // }
+
+//Question#03
+  // int total_class = 16;
+  // int class_attend = 14;
+  // num percentage;
+  // percentage = (class_attend / total_class) * 100;
+  // if (percentage >= 75) {
+  //   print("Student are allowed to Sit in exam  ${percentage}");
+  // } 
+  // else {
+  //   print("Student are not allowed to Sit in exam  ${percentage}");
+  // }
+
+//Question#05
+// num temp=15;
+//   if(temp<0)
+//   {
+//     print("Its Freezing Weather ");
+//   }
+//   else if (temp < 10)
+//   {
+//     print("the very Cold Weather");
+//   }
+//   else if (temp< 20)
+//   { 
+//     print("Cold Weather");
+//    }
+//   else if(temp<30)
+//   {
+//     print("Normal Temprature");
+//   }
+//  else if (temp<40)
+//  {
+//    print("Its Hot");
+//  }
+//   else if (temp>=40)
+//   {
+//     print("Very Hot Weather");
+//   }
+//Question#06
+  // var cha = 'b';
+  // if (cha == 'a' ||
+  //     cha == 'e' ||
+  //     cha == 'i' ||
+  //     cha == 'o' ||
+  //     cha == 'u' ||
+  //     cha == 'A' ||
+  //     cha == 'E' ||
+  //     cha == 'I' ||
+  //     cha == 'O' ||
+  //     cha == 'U') {
+  //   print("This is Vowel ${cha} ");
+  // } else {
+  //   print("This is Consonents ${cha}");
+  // }
+//Question#07
+//  var coustomer_id=1001;
+//   var coustomer_name="Muhammad Faisal Jaweed ";
+//   num unit=800;
+//   print("Coustomer name : $coustomer_name");
+//   print("Coustomer Id : $coustomer_id");
+//   print("Unit Consumed : $unit");
+//   if(unit<199)
+//   {
+//     print("Amount Charges @Rs. 2.00 per unit:${1.20*unit}");
+//   }
+//   else if (unit>=200  && unit<=400 )
+//   {
+//     print("Amount Charges @Rs. 2.00 per unit:${1.50*unit}");
+//   }
+//   else if(unit>400 && unit<=600)
+//   {
+//     print("Amount Charges @Rs. 2.00 per unit:${ 1.80*unit}");
+//   }
+//   else if (unit >600)
+//   {
+//     print("Amount Charges @Rs. 2.00 per unit:${2.0*unit}");
+//   }
+}
